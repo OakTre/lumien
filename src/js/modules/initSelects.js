d02369d7cf459bdcf40197d8f817430a4fbb36c3
OakTre <nailgalimov8@gmail.com>
2022-05-19 13:08:08 +0300

    upd

--- src/js/modules/initSelects.js
+++ src/js/modules/initSelects.js
@@ -6,21 +6,57 @@ export default function initSelects() {
   const clearBtns = document.querySelectorAll(".js-choises-clear");
   let choices = [];
 
-  if (document.querySelectorAll('.js-custom-select')) {
+  if (customSelects.length) {
     customSelects.forEach((select) => {
-      let slct = new Choices(select, {
+      const input = select.closest('.custom-select') ? select.closest('.custom-select').querySelector('input') : null;
+
+      const slct = new Choices(select, {
         searchEnabled: false,
         itemSelectText: '',
         shouldSort: false,
-				allowHTML: true,
+        allowHTML: true,
+        renderSelectedChoices: 'always',
       });
+
+      // 😭😭😭😭😭
+      if (select.hasAttribute('multiple')) {
+        slct.passedElement.element.addEventListener('choice', function (event) {
+          slct.getValue(true).forEach(item => {
+            if (item !== event.detail.choice.value) return;
+            setTimeout(() => {
+              slct.removeActiveItemsByValue(event.detail.choice.value);
+              setValue(slct, input);
+            }, 100)
+          });
+        });
+
+        slct.passedElement.element.addEventListener('change', () => {
+          setValue(slct, input);
+        })
+
+        setValue(slct, input);
+
+        const multipleLabel = select.dataset.name || "тест";
+        select.parentElement.textContent = multipleLabel;
+      }
+
+      function setValue(choice, input) {
+        if (input) {
+          input.value = choice.getValue(true).length ? JSON.stringify(choice.getValue(true)) : "";
+          const evt = document.createEvent('HTMLEvents');
+          evt.initEvent('input', true, true);
+          input.dispatchEvent(evt);
+
+          console.log(input.value);
+        }
+      }
 
       choices.push(slct);
     });
 
-    clearBtns.forEach((btn)=> {
-      btn.addEventListener("click", ()=>{
-        choices.forEach((slct)=>{
+    clearBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        choices.forEach((slct) => {
           slct.destroy();
           slct.init();
         });
